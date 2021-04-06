b9d1a02c8054c8b7be3162f98b4797bca6230aed
selahssea <>
2021-04-06 02:58:45 -0700

    Simplify cleanup

--- cleanup.py
+++ cleanup.py
@@ -3,25 +3,20 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-pins = [
+PINS = [
     14,  # 1
     15,  # 2
     18,  # 4
     23,  # 8
 ]
 
-def setup_pins():
-    for p in (pins):
+def setup_and_off_pins():
+    for p in (PINS):
         GPIO.setup(p, GPIO.OUT)
         print('Pin', p, ' enabled for OUT')
-
-def off_all_pins():
-    for p in (pins):
         GPIO.output(p, GPIO.LOW)
         print('Pin ', p, ' OFF')
 
-setup_pins()
-
-off_all_pins()
+setup_and_off_pins()
 
 GPIO.cleanup()
